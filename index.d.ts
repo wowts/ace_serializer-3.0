db9657505051fd4527be06bc5ffd85ac8eb1636d
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 13:26:16 +0100

    Clean

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Library } from "@wowts/tslib";
-export interface AceSerializer {
-    Serialize(...args: any[]): string;
-    Deserialize(messsage: string): any[];
-}
-declare const lib: Library<AceSerializer>;
-export default lib;
